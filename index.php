a76241e663edbac05fa61ce444e0d21ce481e6e4
Your Name <you@example.com>
2017-10-05 00:16:50 -0400

    Replaced - with /

--- index.php
+++ index.php
@@ -4,7 +4,10 @@ $obj = new main();
 
 class main
 {
-public function __construct()
+
+	// private $html; 
+
+	public function __construct()
 	{
 
 		$date =  date('Y-m-d', time());
@@ -16,10 +19,40 @@ public function __construct()
 		$year = array("2012", "396", "300","2000", "1100", "1089");
 		echo "The value of \$year: ";
 		print_r($year);
-	}
-	public function __destruct()
-	{
-	echo"First commit";
-	}
+
+		echo htmlTags::breakLine();
+
+		echo htmlTags::headingOne("Replace - with /");
+		$date = stringFunction::stringreplace("-","/",$date);
+		echo $date;
+		echo htmlTags::breakLine();
+		echo htmlTags::horizontalline();
+     }
+
 }
-?>
+class stringFunction
+{
+
+	 static public function stringreplace($src,$tar,$string)
+	 {
+	 	$string = str_replace($src,$tar,$string);
+	 	return $string;
+	 }
+}
+class htmlTags
+{
+  static public function horizontalline() 
+  {
+      return '<hr>';
+   }
+    static public function headingOne($text) 
+    {
+      return '<h1>' . $text . '</h1>';
+    }
+    static public function breakLine()
+    {
+    	return '<br>';
+    }
+ }
+
+
